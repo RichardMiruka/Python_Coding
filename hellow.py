5d66d91263017bd14e09c6d45251dd179f59cc70
RichardMiruka <richardonsare15@gmail.com>
2024-11-13 14:58:16 +0300

    using methods in a function

--- hellow.py
+++ hellow.py
@@ -4,4 +4,3 @@ name = input ("What is your name? ") # ask the user for their name using the inp
 name = name.strip()                  # remove the space before and after the name using the strip() method
 name = name.capitalize()             # capitalize the first letter of the name using the capitalize() method
 print(f"Hello, {name}!")             # print the greeting message with the user's name
-
