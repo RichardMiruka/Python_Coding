d0d045320ed74bf8e4136889275aef1a8560d699
RichardMiruka <richardonsare15@gmail.com>
2024-11-13 15:57:01 +0300

    using methods in a function

---
+++ calculator.py
@@ -0,0 +1,19 @@
+"""
+x = 1
+y = 2
+
+z = x + y
+print(z) # Output: 3
+"""
+
+x = input ("What is X? ")
+y = input ("What is Y? ")
+
+# z = x + y      # Output: 12
+z = int(x) + int(y)
+print(z)
+
+# best way to write the code is to convert the input to an integer directly (nesting the int() function inside the input() function)
+# x = int(input("What is X? "))
+# y = int(input("What is Y? "))
+# print(x + y) # Output: 3
--- hellow.py
+++ hellow.py
@@ -4,3 +4,9 @@ name = input ("What is your name? ") # ask the user for their name using the inp
 name = name.strip()                  # remove the space before and after the name using the strip() method
 name = name.capitalize()             # capitalize the first letter of the name using the capitalize() method
 print(f"Hello, {name}!")             # print the greeting message with the user's name
+
+# name = name.strip().title()   # combine the strip() and title() methods to remove leading and trailing spaces and capitalize the first letter of each word
+
+# The best way to write the code 
+# name = input("Enter your name: ").strip().title()
+# print(f"Hello, {name}!")
