ef1d5f4e92574cbe955f8c516df2f5e0a25bb7b3
RichardMiruka <richardonsare15@gmail.com>
2024-11-14 13:49:36 +0300

    simple function def & calling the function

---
+++ Python_basics/functions.py
@@ -0,0 +1,3 @@
+def hello():                  # Define a function called hello
+    print("Hello World!")     # Print "Hello World!" when the function is called
+hello()                       # Call the function to print "Hello World!"
