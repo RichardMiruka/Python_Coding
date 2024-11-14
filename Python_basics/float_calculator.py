7917e41225689c381cce055856d18cc572640ae8
RichardMiruka <richardonsare15@gmail.com>
2024-11-14 12:48:30 +0300

    functionality to input two numbers, calculate their sum, and display the result rounded to two decimal places.

--- Python_basics/float_calculator.py
+++ Python_basics/float_calculator.py
@@ -6,4 +6,4 @@ print(x + y)
 x = float (input ("What is X? "))
 y = float (input ("What is Y? "))
 z = round (x + y, 2)                 # Round the result to 2 decimal places
-print(z) 
+print(f"{z:.2f}")                    # Display the result with 2 decimal places
