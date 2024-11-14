ab53f35abe338de09233c03ad9b16ed09060680c
RichardMiruka <richardonsare15@gmail.com>
2024-11-14 11:17:30 +0300

    rounding off float integers

--- Python_basics/float_calculator.py
+++ Python_basics/float_calculator.py
@@ -2,3 +2,8 @@ x = float (input ("What is X? "))
 y = float (input ("What is Y? "))
 
 print(x + y)
+
+x = float (input ("What is X? "))
+y = float (input ("What is Y? "))
+z = round (x + y, 2)                 # Round the result to 2 decimal places
+print(z) 
