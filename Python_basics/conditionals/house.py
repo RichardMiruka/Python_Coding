726c79a7b085df8bd479d27c20ab18e9164d06e2
RichardMiruka <richardonsare15@gmail.com>
2024-11-20 15:18:08 +0300

    using match statement in comparison to redundancy

--- Python_basics/conditionals/house.py
+++ Python_basics/conditionals/house.py
@@ -1,6 +1,7 @@
-# The code above uses a series of if-elif-else statements to
-#  compare the value of the variable name against different patterns.
-name = input("What is your name?")  # Prompt the user to input their name
+# The code below uses a series of if-elif-else statements to
+# compare the value of the variable name against different patterns.
+
+name = input("What is your name? ") # Prompt the user to input their name
 if name == "Harry":                 # Check if the name is "Harry"
     print("Gryffindor")             # If the name is "Harry", print "Gryffindor"
 elif name == "Hermione":            # Check if the name is "Hermione"
@@ -12,24 +13,43 @@ elif name == "Draco":               # Check if the name is "Draco"
 else:                               # If the name does not match any of the above conditions
     print("Unknown")                # Print "Unknown"
 
+# reduce reduncancy in the code above  using the following code 
+name = input ("what is your name? ")
+if name =="Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Unknown")
 
 
+# The match statement is used to compare the value of house against different patterns
 # and execute the corresponding block of code based on the matching pattern.
 # In this example, the value "house" is matched against different house names
 # from the Harry Potter series, and the corresponding house name is printed to the console. 
 # If the value does not match any of the specified patterns, the default case (_) is executed,
 # printing "Unknown" to the console.
 
-# using the match statement to compare the value of house against different patterns
-match house:
-    case "Gryffindor":
-        print("Gryffindor")
-    case "Hufflepuff":
-        print("Hufflepuff")
-    case "Ravenclaw":
-        print("Ravenclaw")
-    case "Slytherin":
-        print("Slytherin")
+name = input ("What is your name? ")
+match name:
+    case "Harry":
+        print ("Gryffindor")
+    case "Hermione":
+        print ("Gryffindor")
+    case "Ron":
+        print ("Gryffindor")
+    case "Draco":
+        print ("Slytherin")
     case _:
-        print("Unknown")
-# The match statement is 
+        print ("Unknown")
+
+# we can iprove the code above by combining the cases that have the same output into a
+# single case statement and using the case keyword to assign the house name to the variable house.
+# This reduces redundancy in the code and makes it more concise.
+
+name = input ("What is your name? ")
+match name:
+    case "Harry", "Hermione", "Ron" => house
+    case "Draco" => house
+    case _ => house = "Unknown"
+print (house)
