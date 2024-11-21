dcd384d4dc4dcc20dbdf5bc772ecffde0a21ad01
RichardMiruka <richardonsare15@gmail.com>
2024-11-21 12:56:16 +0300

    script to check internet speed using Python

--- important_scripts/check_internet_speed.py
+++ important_scripts/check_internet_speed.py
@@ -1,23 +1,25 @@
-# check internet spped using python
+# Check internet speed using Python
 
 # pip install speedtest-cli - Import the speedtest module from the speedtest-cli library
-
-import speedtest as st
+import speedtest
 
 def check_internet_speed():                               # Define a function to check the internet speed
-    test = st.Speedtest()                                 # Initialize a Speedtest object
+    test = speedtest.Speedtest()                         # Initialize a Speedtest object
 
     # Perform the download speed test and convert the result to Mbps
-    download_speed = test.download()                      # Get the download speed in bytes
-    download_speed = round(download_speed / 10**6, 2)     # Convert from bits per second to megabits per second
-    print(f"Download Speed: {download_speed} Mbps")       # Print the download speed
+    download_speed = test.download()                     # Get the download speed in bits per second
+    download_speed = round(download_speed / 10**6, 2)    # Convert from bits per second to megabits per second
+    print(f"Download Speed: {download_speed} Mbps")      # Print the download speed
 
     # Perform the upload speed test and convert the result to Mbps
-    upload_speed = test.upload()
-    upload_speed = round(upload_speed / 10**6, 2)          # Convert from bits per second to megabits per second
-    print(f"Upload Speed: {upload_speed} Mbps")            # Print the upload speed
+    upload_speed = test.upload()                         # Get the upload speed in bits per second
+    upload_speed = round(upload_speed / 10**6, 2)        # Convert from bits per second to megabits per second
+    print(f"Upload Speed: {upload_speed} Mbps")          # Print the upload speed
 
     # Get the ping (latency) result in milliseconds
-    ping = test.results.ping
-    print(f"Ping: {ping} ms")
-check_internet_speed()                                      # Call the function to check the internet speed
+    test.get_best_server()                               # Identify the best server for accurate ping measurement
+    ping = test.results.ping                             # Get the ping value in milliseconds
+    print(f"Ping: {ping} ms")                            # Print the ping value
+
+# Call the function to check the internet speed
+check_internet_speed()
