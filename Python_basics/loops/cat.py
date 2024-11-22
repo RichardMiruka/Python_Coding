77b629034873f244d33ba9b58a9513a44e68732b
RichardMiruka <richardonsare15@gmail.com>
2024-11-22 14:45:17 +0300

    This script prints 'meow' three times using a while loop

--- Python_basics/loops/cat.py
+++ Python_basics/loops/cat.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python
+"""
+#!/usr/bin/python
+# This is called a "shebang" line. It specifies the interpreter to use for executing the script.
+# In this case, it points to the Python interpreter located at /usr/bin/python.
+
+i = 0
+# Initialize a variable `i` with the value 0. This variable will act as a counter for the loop.
+
+while i < 3:
+    # Start a while loop that will run as long as the condition `i < 3` is True.
+    # This means the loop will execute when `i` is 0, 1, or 2.
+
+    print("meow")
+    # Print the string "meow" to the console on each iteration of the loop.
+
+    i += 1
+    # Increment the value of `i` by 1 on each iteration.
+    # This ensures the loop will eventually stop when `i` reaches 3.
+    """
 
 i = 0
 while i < 3:
