95e6ea00e0d0eae6c3d2efae042f9374c36eb905
RichardMiruka <richardonsare15@gmail.com>
2024-11-22 14:26:59 +0300

    fixed pycodestyle errors

--- Python_basics/conditionals/compare.py
+++ Python_basics/conditionals/compare.py
@@ -1,7 +1,7 @@
 x = int(input("What is x? "))
-y = int(input("What is y?" ))
+y = int(input("What is y?"))
 
-if x > y :
+if x > y:
     print(f"{x} is greater than {y}")
 elif x < y:
     print(f"{x} is less than {y}")
