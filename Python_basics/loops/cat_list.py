9f5fe0420819132a05b762ef8fae4e462869ab95
RichardMiruka <richardonsare15@gmail.com>
2024-11-22 15:26:51 +0300

    This script prints 'meow' ten times using a for loop

---
+++ Python_basics/loops/cat_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+This script prints 'meow' ten times using a for loop.
+It demonstrates the basics of loops, iteration, and printing.
+"""
+# Loop through a range of numbers from 0 to 9 (10 iterations)
+for i in range(10):
+    print("meow")
