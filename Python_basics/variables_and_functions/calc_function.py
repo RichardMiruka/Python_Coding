df2fd3cda0d311aee48f19522f1140c5cd9d6b74
RichardMiruka <richardonsare15@gmail.com>
2024-11-23 23:05:57 +0300

    pycodestyle implementation

--- Python_basics/variables_and_functions/calc_function.py
+++ Python_basics/variables_and_functions/calc_function.py
@@ -1,9 +1,15 @@
 def main():
-    x = int (input ("What is X? "))
+    x = int(input("What is X? "))
     # print ("X squared is", x*x)
 
     # or use the square function
-    print ("X squared is", square(x))
-def square(n):                        # Define a function called square that takes a number as input and returns the square of that number
-    return n*n
+    print("X squared is", square(x))
+
+
+def square(
+    n,
+):  # Define a function called square that takes a number as input and returns the square of that number
+    return n * n
+
+
 main()
